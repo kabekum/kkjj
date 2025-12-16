64cacd9bb151d90d1c70155a69cd889a91d4c2f0
kabekum <98370249+kabekum@users.noreply.github.com>
2025-12-17 00:45:43 +0300

    Update SECRET_KEY to remove prefix

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 #FIXME
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY=3m9@#shhj2%o9bl3hbvqjsk3d1o83dys7f7xrd8*+q96nsh#vq'
+SECRET_KEY = '3m9@#shhj2%o9bl3hbvqjsk3d1o83dys7f7xrd8*+q96nsh#vq'
 
 DEBUG = True
 
